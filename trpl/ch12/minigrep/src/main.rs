738f4c78edf6a6769143ee898a7c1587cebbd8fa
endixk <endix1029@gmail.com>
2023-01-22 21:28:27 +0900

    minigrep for crates.io

--- trpl/ch12/minigrep/src/main.rs
+++ trpl/ch12/minigrep/src/main.rs
@@ -1,19 +1,17 @@
-extern crate greprs;
+extern crate endixk_minigrep as minigrep;
 
 use std::env;
 use std::process;
 
-use greprs::Config;
+use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = greprs::run(config) {
+    if let Err(e) = minigrep::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
